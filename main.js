8204f8d673fa60deed8edeb333a6aa0dd223555a
arifuldev788 <arifulbiswas787@gmail.com>
2022-08-10 00:57:53 -0700

    new

--- main.js
+++ main.js
@@ -3,16 +3,25 @@ loginBtn.addEventListener('click', function(){
     document.getElementById('login-area').style.display = 'none';
     document.getElementById('trasition-area').style.display = 'block';
 })
-        
+    //   Diposit Amount Input  
 const dipositBtn = document.getElementById('diposit-sub');
 dipositBtn.addEventListener('click', function(){
-    const dipositAmount = document.getElementById('diposit-amount').value;
-    const dipositNumber = parseFloat(dipositAmount);
-
+    const dipositNumber = getInputValue('diposit-amount');
     updateSpanText("current-diposit", dipositNumber);
     updateSpanText("current-balance", dipositNumber);
     emptyInput('diposit-amount');
 })
+//   Diposit Amount Input End 
+
+//   Withdraw Amount Input start 
+const withdrawBtn = document.getElementById('withdraw-btn');
+withdrawBtn.addEventListener('click', function(){
+    const withdrawNumber = getInputValue('withdraw-amount');
+    updateSpanText("current-withdraw", withdrawNumber);
+    updateSpanText("current-balance", -1 * withdrawNumber);
+    emptyInput('withdraw-amount');
+})
+//   Withdraw Amount Input end
 
 function updateSpanText(id, dipositNumber){
     const current = document.getElementById(id).innerText;
@@ -20,18 +29,12 @@ function updateSpanText(id, dipositNumber){
     const total = dipositNumber + currentNumber;
     document.getElementById(id).innerText = total;
 }
+function getInputValue(id){
+    const inputValue = document.getElementById(id).value;
+    const inputNumber = parseFloat(inputValue);
+    return inputNumber;
+}
 
-const withdrawBtn = document.getElementById('withdraw-btn');
-withdrawBtn.addEventListener('click', function(){
-    const withdrawAmount = document.getElementById('withdraw-amount').value;
-    const withdrawNumber = parseFloat(withdrawAmount);
-    updateSpanText("current-withdraw", withdrawNumber);
-    const currentBalance = document.getElementById('current-balance').innerText;
-    const currentBalanceNumber = parseFloat(currentBalance);
-    const netBalance = currentBalanceNumber - withdrawNumber;
-    document.getElementById('current-balance').innerText = netBalance;
-    emptyInput('withdraw-amount');
-})
 function emptyInput(id){
     document.getElementById(id).value = "";
 }
